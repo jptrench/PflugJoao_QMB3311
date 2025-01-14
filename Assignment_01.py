813c2b3ff82b92e425f62b849ba3bcb53164908e
jptrench <jo252345@ucf.edu>
2025-01-13 21:56:38 -0500

    Create Assignment_01.py

---
+++ Assignment_01.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 11:38:11 2025
+
+@author: jptpf
+"""
+
+#Assignment 01
+#part a
+number = 2
+number
+
+#part b
+number *= 2
+number
+
+#part c
+number += 5
+number
+
+#part d
+number *= 50
+number
+
+#part e
+number += 1749
+number
+
+#part f
+number += 25
+number
+
+#part g 
+number -= 2001
+number
+
+#part h
+result = number
+result
